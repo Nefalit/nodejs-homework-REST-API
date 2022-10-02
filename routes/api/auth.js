4e03b676eee80321cdc49c15ccb9d0aee6f3b7b6
Nefalit <nefalit7@gmail.com>
2022-10-02 19:30:06 +0300

    mb done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,11 @@ const ctrl = require('../../controllers/auth');
 
 const { ctrlWrapper } = require('../../helpers');
 
-const { validateContactBody, authenticate } = require('../../middlewares');
+const {
+  validateContactBody,
+  authenticate,
+  upload,
+} = require('../../middlewares');
 
 const { schemas } = require('../../models/user');
 
@@ -27,5 +31,12 @@ router.post(
 router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent));
 
 router.get('/logout', authenticate, ctrlWrapper(ctrl.logout));
+
+router.patch(
+  '/avatars',
+  authenticate,
+  upload.single('avatar'),
+  ctrlWrapper(ctrl.updateAvatar)
+);
 
 module.exports = router;
